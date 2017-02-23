b0ff9af42cbdceef9ff648f78cfaad501d7f5b17
Matthias Löffel <matthias.loeffel@gmail.com>
2017-02-23 14:37:30 +0100

    c! missing exported symbols

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
-export { IsotopeModule } from './src/module';
 import { AngularIsotope } from './src/isotope';
 import { AngularIsotopeBrick } from './src/brick';
+export { IsotopeModule } from './src/module';
+export { AngularIsotope } from './src/isotope';
+export { AngularIsotopeBrick } from './src/brick';
 export { IsotopeOptions } from './src/isotope-options';
 
 export const ISOTOPE_DIRECTIVES = [
